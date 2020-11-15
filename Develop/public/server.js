f5469de6584f4a3193d945e15df6574d378ad0a4
Kevin McAteer <kmcateer926@gmail.com>
2020-11-15 14:38:10 -0500

    added api routes

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -15,7 +15,15 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/notes.html"));
   });
 
+  app.get("/api/notes", (req, res) => {
+    res.json({
+      notes: notes,
+    });
+  });
   
+  app.post("/api/notes", (req, res) => {
+      
+  })
 
 app.listen(PORT, () => {
     console.log(`App is running on http://localhost:${PORT}`);
