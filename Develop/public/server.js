5a42987af62c560d616ee94d2c4c8b6831396dec
Kevin McAteer <kmcateer926@gmail.com>
2020-11-15 14:29:58 -0500

    added paths for notes.html and index.html

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -8,6 +8,14 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/index.html"));
+  });
+  app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/notes.html"));
+  });
+
+  
 
 app.listen(PORT, () => {
     console.log(`App is running on http://localhost:${PORT}`);
