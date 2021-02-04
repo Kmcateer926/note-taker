62eb8e7dd9431e4c898e2f16b1e867a0fc044adf
Kevin McAteer <kmcateer926@gmail.com>
2021-02-03 21:38:33 -0500

    fully functional

--- server.js
+++ server.js
@@ -1,82 +1,80 @@
-
-//1. Require Express
+// require express and calling express
 const express = require("express");
-const fs = require("fs");
-const { appendFile } = require("fs");
-const path = require("path");
-const { v4: uuidv4 } = require('uuid');
-
-//2.Create Instance of Express
-
 const app = express();
-
-//3. Define the PORT
+// creating a PORT
 const PORT = process.env.PORT || 8080;
+// requiring path and fs
+const path = require("path");
+const fs = require("fs");
+// requiring uuid for the unique id package
+const { v4: uuidv4 } = require("uuid");
+const notesArray = [];
 
-const notes =[{
-    title: "Groceries",
-    text: "Apples, Bananas, Cherries"
-}]
-//6. Middleware
+// dynamic middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Link CSS and JS
-app.get("/assets/js/index.js", (req,res) => {
-    res.sendFile(path.join(__dirname, "/public/assets/js/index.js"));
+// static middleware
+app.use(express.static("public"));
+
+// html routes / notes route
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/views/notes.html"));
 });
 
-app.get("/assets/css/styles.css", (req,res) => {
-    res.sendFile(path.join(__dirname, "/public/assets/css/styles.css"));
+// html route for the catch all or the index in this app
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/views/index.html"));
 });
 
-//5. Add a route
-
-    //View Routes
-app.get("/notes", (req,res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-//API Routing  
-    //GET ROUTE
-    app.get("/api/notes", (req,res) => {
-    fs.readFile("./db/db.json", (err,data) => {
-        if (err) throw err;
-        const previousNotes = JSON.parse(data);
-        return res.json(previousNotes);
-    });
+// API ROUTES/
+app.get("/api/config", (req, res) => {
+  return res.json({
+    success: true,
+  });
 });
 
-    //POST ROUTE
-app.post("/api/notes", (req,res) => {
-    let previousNotes = fs.readFileSync("./db/db.json");
-    req.body.id = uuidv4();
-    previousNotes = JSON.parse(previousNotes);
-    previousNotes.push(req.body);
-    previousNotes=JSON.stringify(previousNotes);
-    fs.writeFileSync("./db/db.json", previousNotes);
-    previousNotes=JSON.parse(previousNotes);
-    return res.json(previousNotes);
-})
-
-    //DELETE route
-app.delete("/api/notes/:id", (req,res) => {
-    let previousNotes = fs.readFileSync("./db/db.json");
-    previousNotes = JSON.parse(previousNotes);
-    previousNotes = previousNotes.filter(function(data){
-        return data.id != req.params.id;
-        });
-    previousNotes = JSON.stringify(previousNotes);
-    fs.writeFileSync("./db/db.json",previousNotes);
-    previousNotes = JSON.parse(previousNotes);
-    return res.send(previousNotes);
-})
-
-app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get("/api/notes", (req, res) => {
+  return res.json(
+    JSON.parse(fs.readFileSync(path.join(__dirname, "/db/db.json")))
+  );
 });
 
-//4. Listen on the port
-app.listen(PORT,() => {
-    console.log(`App listening on http://localhost:${PORT}`);
+app.get("/api/notes:id", (req, res) => {
+  res.json(data, req.params.id);
+});
+
+// post
+app.post("/api/notes", (req, res) => {
+  let createNewNote = req.body;
+
+  createNewNote.id = uuidv4();
+
+  const notes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "./db/db.json"))
+  );
+
+  notes.push(createNewNote);
+
+  fs.writeFileSync(path.join(__dirname, "./db/db.json"), JSON.stringify(notes));
+
+  return res.json(notes);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+
+  const theNotes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "./db/db.json"))
+  );
+
+  let notesNotToDelete = theNotes.filter((note) => note.id !== id);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(notesNotToDelete));
+
+  return res.json(notesNotToDelete);
 });
